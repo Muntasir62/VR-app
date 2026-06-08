4a84a1965c0aa40b2bcda9864591ca365b5a85d5
Muntasir62 <muntasiralamayon@gmail.com>
2026-06-08 20:02:53 +0600

    commit

--- test-db.js
+++ test-db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri ="mongodb://muntasiralamayon_db_user:icl5ms4hoNBwTmSp@ac-uglibjf-shard-00-00.6kj5fe9.mongodb.net:27017,ac-uglibjf-shard-00-01.6kj5fe9.mongodb.net:27017,ac-uglibjf-shard-00-02.6kj5fe9.mongodb.net:27017/techdojo_vr_db?ssl=true&replicaSet=atlas-b8h5fh-shard-0&authSource=admin&appName=Cluster1";
+const uri =process.env.MONGODB_URI;
 
 async function run() {
   try {
